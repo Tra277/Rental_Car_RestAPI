9ae0797683f43f508aff87aa670f0ac482a2572c
Tra277 <sontra2773@gmail.com>
2024-12-04 14:12:46 +0700

    Initial Commit, Base Project

---
+++ src/main/java/com/tsubakistudio/RentalCarBE/RentalCarBeApplication.java
@@ -0,0 +1,13 @@
+package com.tsubakistudio.RentalCarBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalCarBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalCarBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tsubakistudio/RentalCarBE/RentalCarBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsubakistudio.RentalCarBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalCarBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
